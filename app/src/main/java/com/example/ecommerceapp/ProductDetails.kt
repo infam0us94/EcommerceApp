e8028ce2110c2e6eb461e4357fc5ac2e369fc71e
infam0us94 <ivanpopoff94@mail.ru>
2020-11-23 22:07:43 +0300

    alert dialog

--- app/src/main/java/com/example/ecommerceapp/ProductDetails.kt
+++ app/src/main/java/com/example/ecommerceapp/ProductDetails.kt
@@ -1,6 +1,8 @@
 package com.example.ecommerceapp
 
+import android.content.DialogInterface
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.product_details.*
 
@@ -12,5 +14,16 @@ class ProductDetails : AppCompatActivity() {
 
         val title = intent.getStringExtra("title")
         product_name.text = title
+
+        availability.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setMessage("It's in stock")
+                .setPositiveButton("Ok", object : DialogInterface.OnClickListener {
+                    override fun onClick(dialog: DialogInterface?, which: Int) {
+                    }
+                })
+                .create()
+                .show()
+        }
     }
 }
